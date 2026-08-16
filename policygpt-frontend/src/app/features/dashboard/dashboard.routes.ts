79559296c191ec00054a760555b4b166d08158c4
Akshat <akshatagrawal.2864@gmail.com>
2026-08-16 18:09:24 +0530

    added role based navbar and improve protected routing

--- policygpt-frontend/src/app/features/dashboard/dashboard.routes.ts
+++ policygpt-frontend/src/app/features/dashboard/dashboard.routes.ts
@@ -1,24 +1,55 @@
 import { Routes } from '@angular/router';
+
 import { Admin } from './admin/admin';
 import { Citizen } from './citizen/citizen';
 import { Official } from './official/official';
 import { Researcher } from './researcher/researcher';
 
+import { roleGuard } from '../../core/guards/role-guard';
+
 export const DASHBOARD_ROUTES: Routes = [
+
+  // =========================
+  // ADMIN
+  // =========================
   {
     path: 'admin',
     component: Admin,
+    // canActivate: [
+    //   roleGuard(['admin'])
+    // ],
   },
+
+  // =========================
+  // CITIZEN
+  // =========================
   {
     path: 'citizen',
     component: Citizen,
+    // canActivate: [
+    //   roleGuard(['citizen'])
+    // ],
   },
+
+  // =========================
+  // GOVERNMENT OFFICIAL
+  // =========================
   {
     path: 'official',
     component: Official,
+    // canActivate: [
+    //   roleGuard(['official'])
+    // ],
   },
+
+  // =========================
+  // RESEARCHER
+  // =========================
   {
     path: 'researcher',
     component: Researcher,
+    // canActivate: [
+    //   roleGuard(['researcher'])
+    // ],
   },
 ];
---
+++ policygpt-frontend/src/app/features/layout/main-layout/main-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../../../shared/components/navbar/navbar';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, Navbar],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.css'
+})
+export class MainLayout {}
