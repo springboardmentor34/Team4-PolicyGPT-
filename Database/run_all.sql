bba3822ca1d5404be520c774921053c16054d685
Aswini-30 <aswiniganesan30@gmail.com>
2026-08-21 19:50:44 +0530

    Update database schema and migration

--- Database/run_all.sql
+++ Database/run_all.sql
@@ -7,6 +7,7 @@
 \i 00_extensions_and_types.sql
 \i tables/users.sql
 \i tables/schemes.sql
+\i tables/policies.sql
 \i tables/eligibility_rules.sql
 \i tables/notifications.sql
 \i tables/feedback.sql
@@ -14,6 +15,5 @@
 \i tables/audit_logs.sql
 \i tables/search_history.sql
 \i tables/applications.sql
-\i tables/policies.sql
 \i tables/policy_versions.sql
 \i tables/saved_policies.sql
